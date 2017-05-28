6f60044ceda942374ebbf41f32be3f54aee1d51b
Technochips <julienfillol2@gmail.com>
2017-05-28 17:57:20 +0200

    added map classes
    it still can't draw right now

--- game.js
+++ game.js
@@ -2,6 +2,18 @@ class Game
 {
 	constructor(canvasId)
 	{
+		this.map = new Maps([
+			new Thing(10, 10, 0, 1, 0)],[
+			new Linedef(0, 1, 1, 0, 0, 0, 0),
+			new Linedef(1, 2, 1, 0, 0, 0, 0),
+			new Linedef(0, 3, 1, 0, 0, 0, 0),
+			new Linedef(2, 4, 1, 0, 0, 0, 0),
+			new Linedef(3, 4, 1, 0, 0, 0, 0)],[
+			new Vertex(5, 15),
+			new Vertex(5, 5),
+			new Vertex(15, 5),
+			new Vertex(15, 15),
+			new Vertex(20, 10)]);
 		this.render = new Render(canvasId);
 	}
 }
---
+++ map.js
@@ -0,0 +1,41 @@
+class Thing
+{
+	constructor(x, y, direction, type, flags)
+	{
+		this.x = x;
+		this.y = y;
+		this.direction = direction;
+		this.type = type;
+		this.flags = flags
+	}
+}
+class Linedef
+{
+	constructor(vert1, vert2, flags, special, sector_tag, front, back)
+	{
+		this.vert1 = vert1;
+		this.vert2 = vert2;
+		this.flags = flags;
+		this.special = special;
+		this.sector_tag = sector_tag;
+		this.front = front;
+		this.back = back;
+	}
+}
+class Vertex
+{
+	constructor(x, y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+}
+class Maps
+{
+	constructor(things, lindefs, vertexes)
+	{
+		this.things = things;
+		this.lindefs = lindefs;
+		this.vertexes = vertexes;
+	}
+}
